c72a18d9406f029597f6275da7a0a01a46f39cfb
Prith Kami <prithvi40201@gmail.com>
2022-04-07 14:29:54 +0530

    basic_auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; // importing component from core package.
+import { FormControl, FormGroup ,Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // This is the root folder where all the components are.
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angfirstapp';
+export class AppComponent 
+{
+  title='login';
+  message:boolean=false;
+  constructor(){
+
+  }
+    login=new FormGroup({
+    email:new FormControl('',[Validators.required, Validators.minLength(6)]),
+    password:new FormControl('',[Validators.required, Validators.minLength(6)]),
+  })
+
+
+  ngOnInit(): void{}
+  
+  submit(){
+    this.message=true;
+  }
 }
